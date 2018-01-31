264d586727114bbefea74041d69602abea23c4d6
aescoda94 <35483827+aescoda94@users.noreply.github.com>
2018-01-31 14:25:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,27 +73,7 @@ def answer(message, roomId):
                  headers=spark_header, data=json.dumps({"roomId":roomId,
                                                       "markdown":message
                                                         }))
-    print("Code after send_message POST: "+str(r.status_code))
-    status= "Message sent to Spark"
-    if r.status_code !=200:
-        print(str(json.loads(r.text)))
-        if   r.status_code == 403:
-            status= "Oops, no soy moderador del team de dicha sala"
-        elif r.status_code == 404:
-            status= "Disculpe. Ya no soy miembro ni moderador de dicho grupo"
-        elif r.status_code == 409:
-            status= "Lo siento, no ha podido ser enviado (409)"
-        elif r.status_code == 500:
-            status= "Perdón, los servidores de Spark están sufriendo problemas.\
-                               Compruébelo aquí: https://status.ciscospark.com/"
-        elif r.status_code == 503:
-            status= "Lo siento. Parece ser que los servidores de Spark no \
-                                            pueden recibir mensajes ahora mismo"
-        else:
-            response = r.json()
-            status= str('Error desconocido: \ '
-                                         + response['errors'][0]['description'])
-    return status
+    return None
 
 def get_displayName (personId,header):
     # To get the displayName of the user, Spark only needs to know the personId
